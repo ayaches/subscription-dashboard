0e3f4b7d91e0d1c7fbd77c31d33814e4b14261f2
Aya Chesmore <ayakiriyama.27@gmail.com>
2026-04-16 13:57:15 +0900

    Add Spring Boot project

---
+++ src/main/java/com/example/subscriptiondashboard/SubscriptionDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptiondashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subscriptiondashboard/SubscriptionDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptiondashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
